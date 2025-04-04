ee35a9f7696cb26019adcdae6cf5494c5350fbfc
Kram-V <markanthonyvivar24@gmail.com>
2025-04-04 13:28:01 +0800

    added helper function to load the .env file

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -1,7 +1,25 @@
 <?php
+function loadEnv($path) {
+  if (!file_exists($path)) return;
+
+  $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+  foreach ($lines as $line) {
+      if (str_starts_with(trim($line), '#')) continue;
+
+      list($key, $value) = explode('=', $line, 2);
+      putenv(trim($key) . '=' . trim($value));
+  }
+}
+
+loadEnv(__DIR__ . '/../.env'); 
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$dbuser = getenv('DB_USER');
+$dbpass = getenv('DB_PASS');
 
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=diary;charset=utf8mb4', 'root', '', [
+  $pdo = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8mb4", $dbuser, $dbpass, [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
   ]);
 }
